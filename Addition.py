252e3e09494662ca8d0f319e60e847c1bfc7e1b1
Pra3496 <79601856+Pra3496@users.noreply.github.com>
2021-02-25 00:51:11 +0530

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,12 @@
+def add(no1,no2):
+   return no1+no2
+
+
+def main():
+   num=int(input("Enter the number"))
+   num1=int(input("Enter the number"))
+   sum=add(num,num1)
+   print("Addition is : ",sum)
+   
+if __name__=="__main__":
+   main()
